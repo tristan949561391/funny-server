872ad2bb3a4af8f827ca26d68d296e397d4cf569
tc949 <tc949561391@hotmai.com>
2017-08-28 15:29:01 +0800

    8.29

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const Koa = require('koa');
 const Koa_logger = require('koa-logger');
 const Koa_parser = require('koa-bodyparser');
-const Koa_error=require('koa-json-error');
-const Koa_cors=require('kcors');
+const Koa_error = require('koa-json-error');
+const Koa_cors = require('kcors');
 module.exports = app = new Koa();
 
 
@@ -11,8 +11,5 @@ app.use(Koa_parser());
 app.use(Koa_error());
 app.use(Koa_cors());
 
-
-app.use(async (ctx) => {
-
-    ctx.body = 'hello world'
-});
+//route list
+app.use(require('./core/route/home.route/index').routes())
